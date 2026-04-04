4c578f9fc56f33996f67926c3d282eb2c0aea333
USER <test@example.com>
2026-04-04 18:38:08 +0000

    Add Created By Darlain link to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,8 +37,11 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-bottom">
-          <p className="footer-copyright">
+          <p className="footer-copyright" style={{ paddingBottom: '0.5rem' }}>
             &copy; {new Date().getFullYear()} All Rights Reserved.
+          </p>
+          <p className="footer-copyright">
+            Created By <a href="https://wa.me/qr/V4R6BT5BX6PPB1" target="_blank" rel="noopener noreferrer" style={{ color: 'inherit', textDecoration: 'underline' }}>Darlain</a>
           </p>
         </div>
       </footer>
